1353d7c0c06032d9e385178e402cab65aa273cc2
ilcm96 <ilcm96@gmail.com>
2020-02-23 21:44:01 +0900

    Update deploy.py

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,12 +3,12 @@ import subprocess
 
 DOCKER_HUB_ID = os.environ["DOCKER_HUB_ID"]
 DOCKER_HUB_PW = os.environ["DOCKER_HUB_PW"]
-DEPOLY_CMD = os.environ["DEPOLY_CMD"]
+SERVER_URL = os.environ["SERVER_URL"]
 
 login_cmd = "docker login -u " + DOCKER_HUB_ID + " -p " + DOCKER_HUB_PW
 build_cmd = "docker build -t ilcm96/neis-api:latest ."
 push_cmd = "docker push ilcm96/neis-api:latest"
-deploy_cmd = str(DEPOLY_CMD)
+deploy_cmd = "curl -s -X POST" + SERVER_URL
 
 subprocess.call(login_cmd, shell=True)
 subprocess.call(build_cmd, shell=True)
